20c282097c6ce7c7848f7ed7e6100ffb426dad0a
Miguel MS <solmurmig10@gmail.com>
2022-12-27 23:48:02 -0600

    Config eslint and prittier

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom";
 import './index.css';
 
-const app = <h1>LearnThis</h1>;
+// const app = <h1>LearnThis</h1>;
+const app = <img src="/vite.svg" />
 const container = document.getElementById('root');
 
 ReactDOM.render(app, container);
